96302a7f04ea53fe6f1a5ac6aa51e7fa68cffada
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-12 07:04:35 -0300

    -Add teste unitário ao CalculoService
    Foi adicionado alguns testes unitários na camada service do Calculo, validando o envio de valor incorretos, e o envio de valores corretos

---
+++ Avaliacao-Atos.Application.Tests/Services/CalculoServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Avaliacao_Atos.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Avaliacao_Atos.Application.Tests.Services
+{
+    public class CalculoServiceTests
+    {
+        private readonly CalculoService calculoService;
+
+        public CalculoServiceTests()
+        {
+            calculoService = new CalculoService();
+        }
+
+        #region validações
+
+        [Fact]
+        public void GetCalculo_SendingInvalidValorMonetario()
+        {
+            var exception = Assert.Throws<Exception>(() => calculoService.GetCalculo(0, 2));
+            Assert.Equal("O valor monetário informado deve ser positivo", exception.Message);
+        }
+
+        [Fact]
+        public void GetCalculo_SendingInvalidQuantidadeMeses()
+        {
+            var exception = Assert.Throws<Exception>(() => calculoService.GetCalculo(100, 1));
+            Assert.Equal("A quantidade de meses deve ser maior que 1", exception.Message);
+        }
+
+        #endregion
+
+        #region validação de parâmetros corretos
+
+        [Fact]
+        public void GetCalculo_SendingValidParameters()
+        {
+            var result = calculoService.GetCalculo(100, 2);
+
+            Assert.True(result.valor_bruto > 0 && result.valor_liquido > 0);
+        }
+
+        #endregion
+    }
+}
